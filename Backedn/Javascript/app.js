f9d8a824e9c4bc49a7a64d2ab109e806da11b399
Juan Florian <juanpabloflorian12@gmail.com>
2023-09-22 21:05:38 -0500

    se agrega carpeta javascript

---
+++ Backedn/Javascript/app.js
@@ -0,0 +1,6 @@
+// variable global = var
+var nombreVariable = "Hola esta es mi variable"
+// para inprimir se usa console.log
+console.log ("ee")
+console.log ("dddd")
+nombreVariable = "segundo nombre de la variable"
